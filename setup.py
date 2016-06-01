ef3b4b7125be4c72dd5c59d74b0db8a27b3aacc5
Erin Sheldon <erin.sheldon@gmail.com>
2016-06-01 09:47:17 -0400

    send the include directories to the Extension constructor, rather than the setup

--- setup.py
+++ setup.py
@@ -3,14 +3,19 @@ from distutils.core import setup, Extension, Command
 import os
 import numpy
 
-ext=Extension("healpix_util._healpix", ["healpix_util/_healpix.c"])
+ext=Extension(
+    "healpix_util._healpix",
+    ["healpix_util/_healpix.c"],
+    include_dirs=[numpy.get_include()],
+)
 
 exec(open('healpix_util/version.py').read())
 
-setup(name="healpix_util", 
-      packages=['healpix_util'],
-      version=__version__,
-      ext_modules=[ext],
-      include_dirs=numpy.get_include())
+setup(
+    name="healpix_util", 
+    packages=['healpix_util'],
+    version=__version__,
+    ext_modules=[ext],
+)
 
 
